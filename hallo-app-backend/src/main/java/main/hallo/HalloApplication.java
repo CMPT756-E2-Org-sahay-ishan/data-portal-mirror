4fc7df85d6c47e4ea88e6606506c629bb9b22277
Alireza Ensan <528-aensan@users.noreply.gitlab.rcg.sfu.ca>
2023-10-17 02:09:01 -0700

    dockerized depolyment for backend, frontend, nginx and prod profile added

--- hallo-app-backend/src/main/java/main/hallo/HalloApplication.java
+++ hallo-app-backend/src/main/java/main/hallo/HalloApplication.java
@@ -7,8 +7,8 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.content.fs.config.EnableFilesystemStores;
-import org.springframework.content.fs.io.FileSystemResourceLoader;
+//import org.springframework.content.fs.config.EnableFilesystemStores;
+//import org.springframework.content.fs.io.FileSystemResourceLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
