c7d894cf427099f23ceb77f218a1a67a039d8990
Alireza Ensan <528-aensan@users.noreply.gitlab.rcg.sfu.ca>
2024-02-06 12:45:41 -0800

    1. Modified volume addresses and properties-prod for smru audio files 2. addressed CORS problem for register new users

---
+++ hallo-app-backend/src/main/java/main/hallo/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package main.hallo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins("http://front-end") // The allowed origins for all endpoints
+        .allowedMethods("GET", "POST", "PUT", "DELETE"); // allowed HTTP methods
+
+    }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.applyPermitDefaultValues();
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
+
